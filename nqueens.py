de92b5c7e7da9392876c520dbedc932771bb896e
Aries Aviles <ane.aviles@gmail.com>
2021-09-23 22:38:44 -0400

    Working

--- nqueens.py
+++ nqueens.py
@@ -5,7 +5,6 @@ from time import time
 from typing import List, Tuple, Callable, TypeVar
 
 import deap
-import matplotlib.pyplot as pyplot
 import numpy
 import pandas
 from IPython.display import display
@@ -74,37 +73,22 @@ def evolution(*,
     # return duration, statistics log, and the fittest individual
     return time() - start_time, log, hall_of_fame[0]
 
-# =========================
-#  Pop Visualizer
-# =========================
-def plot_generations(generation: List[int], average: List[float], minimum: List[int], maximum: List[int]):
-    pyplot.figure(figsize=(16, 8))
-    pyplot.grid(True)
-    pyplot.plot(generation, average, label="average")
-    pyplot.plot(generation, minimum, label="minimum")
-    pyplot.plot(generation, maximum, label="maximum")
-    pyplot.xlabel("Generation")
-    pyplot.ylabel("Fitness")
-    pyplot.ylim(-1)
-    pyplot.legend(loc="upper right")
-    pyplot.show()
-
 
 # =========================
-#  Rendering
+#  Draw Board
 # =========================
-def display_positional_grid(individual: List[int]):
-    # unpack dimensions
-    dimension = len(individual)
+def display_positional_grid(queen_positions: List[int]):
+    # set dimensions to specified number of
+    dimension = len(queen_positions)
 
     # construct board using pandas
     board = pandas.DataFrame("", index = range(1, dimension + 1), columns = range(1, dimension + 1))
 
-    # draw all conflicts with a red line
+    # draw conflicts within range to n
     for x in range(dimension):
-        x_row, x_column = individual[x] // dimension, individual[x] % dimension
+        x_row, x_column = queen_positions[x] // dimension, queen_positions[x] % dimension
         for y in range(x + 1, dimension):
-            y_row, y_column = individual[y] // dimension, individual[y] % dimension
+            y_row, y_column = queen_positions[y] // dimension, queen_positions[y] % dimension
 
             diff_row, diff_column = y_row - x_row, y_column - x_column
             # check if queens are conflicting
@@ -113,8 +97,9 @@ def display_positional_grid(individual: List[int]):
                 for i in range(1 + max(abs(diff_row), abs(diff_column))):
                     board[1 + x_column + i * numpy.sign(diff_column)][1 + x_row + i * numpy.sign(diff_row)] = "x"
 
+    # reliant on conflicts
     # draw all queens
-    for queen in individual:
+    for queen in queen_positions:
         row, column = queen // dimension, queen % dimension
         # use a crown if
         board[1 + column][1 + row] = "Q" if board[1 + column][1 + row] == "" else "q"
@@ -126,8 +111,8 @@ def display_positional_grid(individual: List[int]):
 #  Example Code
 # =========================
 example_individual = random.sample(range(8**2), 8)
-print(f"Positions: {example_individual}")
-print(f"Duplicate positions: {len(example_individual) - len(set(example_individual))}")
+# print(f"Positions: {example_individual}")
+# print(f"Duplicate positions: {len(example_individual) - len(set(example_individual))}")
 display_positional_grid(example_individual)
 
 # =========================
@@ -153,20 +138,19 @@ def evaluate_position_indexed_fitness(individual: List[int]) -> float:
 # =========================
 #  N = 16
 # =========================
-duration, log, fittest_individual_p8 = evolution(
-    individual_generator=lambda: random.choices(range(8**2), k=8),
+duration, log, fittest_individual_p5 = evolution(
+    individual_generator=lambda: random.choices(range(25**2), k=25),
     fitness_evaluation=evaluate_position_indexed_fitness,
-    population_size=2500,
+    population_size=500,
     generations=100,
     crossover_rate=.5,
     mutation_rate=.5,
     mutation_function=(tools.mutUniformInt, {"low": 0, "up": 8**2 - 1, "indpb": 1/4})
 )
 print(f"Computed in {duration:.3f} seconds")
-plot_generations(*log.select("gen", "avg", "min", "max"))
 
 # Render
-print(f"Positions: {fittest_individual_p16}")
-print(f"Duplicate queens: {len(fittest_individual_p16) - len(set(fittest_individual_p16))}")
-print(f"Fitness: {abs(fittest_individual_p16.fitness.values[0])}")
-display_positional_grid(fittest_individual_p16)
+print(f"Positions: {fittest_individual_p5}")
+print(f"Duplicate queens: {len(fittest_individual_p5) - len(set(fittest_individual_p5))}")
+print(f"Fitness: {abs(fittest_individual_p5.fitness.values[0])}")
+display_positional_grid(fittest_individual_p5)
---
+++ tsp.py
@@ -0,0 +1,164 @@
+import os
+import pickle
+import array
+import csv
+import codecs
+import random
+from urllib.request import urlopen
+from deap import base
+from deap import creator
+from deap import tools
+from deap import algorithms
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+URL_PREFIX = 'http://elib.zib.de/pub/mp-testdata/tsp/tsplib/tsp/'
+
+# =========================
+#  Read Data
+# =========================
+def read_tsp_data():
+    locations = []
+    with open("TSPDATA.txt", "r") as f:
+        for i in range(2):
+            next(f)
+        content = [x.strip('\n')[5:] for x in f.readlines()]
+        col_num = 1
+        for row in content:
+            # delete the index, we need the coordinates only
+            locations.append(np.asarray([row.split()[col_num-1], row.split()[col_num]], dtype=np.int32))
+
+    # calculate distances using vector norm
+    number_cities = len(locations)
+    distances = [[0] * number_cities for _ in locations]
+    for i in range(number_cities):
+        for j in range(i + 1, number_cities):
+            # vector norm
+            distance = np.linalg.norm(locations[j] - locations[i])
+            distances[i][j] = distances[j][i] = distance
+
+    print(f"================================================================\nlocations: {locations}")
+    return locations, distances
+
+# =========================
+#  Set Parameters
+# =========================
+CITIES, DISTANCES = read_tsp_data()
+NUMBER_CITIES = len(CITIES)
+NUM_GENERATIONS = 3000
+POPULATION_SIZE = 100
+P_CROSSOVER = 0.9
+P_MUTATION = 0.1
+
+individual = list(range(NUMBER_CITIES))
+print(f"individ: {individual}")
+individual = random.sample(individual, len(individual))
+print(individual)
+
+# =========================
+#  Calculate Distances
+# =========================
+def tsp_distance(individual: list) -> float:
+    """
+    Returns the traveling distance for particular ordering of cities.
+    :param individual: an ordered list of cities to visit.
+    :return: the total travelled distance.
+    """
+    # get distance between first and last city
+    distance = DISTANCES[individual[0]][individual[-1]]
+    # add all other distances
+    for i in range(NUMBER_CITIES - 1):
+        distance += DISTANCES[individual[i]][individual[i + 1]]
+    return distance
+
+creator.create('FitnessMin', base.Fitness, weights=(-1.0,))
+creator.create('Individual', array.array, typecode='i', fitness=creator.FitnessMin)
+toolbox = base.Toolbox()
+# Create operator to shuffle the cities
+toolbox.register('randomOrder', random.sample, range(NUMBER_CITIES), NUMBER_CITIES)
+# Create initial random individual operator
+toolbox.register('individualCreator', tools.initIterate, creator.Individual, toolbox.randomOrder)
+# Create random population operator
+toolbox.register('populationCreator', tools.initRepeat, list, toolbox.individualCreator)
+
+def tspFitness(individual) -> tuple:
+    return tsp_distance(individual),
+
+toolbox.register('evaluate', tspFitness)
+toolbox.register('select', tools.selTournament, tournsize=3)
+toolbox.register('mate', tools.cxOrdered)
+toolbox.register('mutate', tools.mutShuffleIndexes, indpb=1.0 / NUMBER_CITIES)
+
+population = toolbox.populationCreator(n=POPULATION_SIZE)
+
+HALL_OF_FAME_SIZE = 10
+hof = tools.HallOfFame(HALL_OF_FAME_SIZE)
+
+stats = tools.Statistics(lambda ind: ind.fitness.values)
+stats.register('min', np.min)
+stats.register('avg', np.mean)
+
+logbook = tools.Logbook()
+logbook.header = ['gen', 'nevals'] + stats.fields
+
+invalid_individuals = [ind for ind in population if not ind.fitness.valid]
+fitnesses = toolbox.map(toolbox.evaluate, invalid_individuals)
+for ind, fit in zip(invalid_individuals, fitnesses):
+    ind.fitness.values = fit
+
+hof.update(population)
+hof_size = len(hof.items)
+
+record = stats.compile(population)
+logbook.record(gen=0, nevals=len(invalid_individuals), **record)
+print(logbook.stream)
+
+for gen in range(1, NUM_GENERATIONS + 1):
+    # Select the next generation individuals
+    offspring = toolbox.select(population, len(population) - hof_size)
+
+    # Vary the pool of individuals
+    offspring = algorithms.varAnd(offspring, toolbox, P_CROSSOVER, P_MUTATION)
+
+    # Evaluate the individuals with an invalid fitness
+    invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+    fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+    for ind, fit in zip(invalid_ind, fitnesses):
+        ind.fitness.values = fit
+
+    # add the best back to population:
+    offspring.extend(hof.items)
+
+    # Update the hall of fame with the generated individuals
+    hof.update(offspring)
+
+    # Replace the current population by the offspring
+    population[:] = offspring
+
+    # Append the current generation statistics to the logbook
+    record = stats.compile(population) if stats else {}
+    logbook.record(gen=gen, nevals=len(invalid_ind), **record)
+    print(logbook.stream)
+
+best = hof.items[0]
+print('Best Fitness = ', best.fitness.values[0])
+plt.figure(1)
+
+# plot genetic flow statistics:
+minFitnessValues, meanFitnessValues = logbook.select("min", "avg")
+plt.figure(2)
+sns.set_style("whitegrid")
+plt.plot(minFitnessValues, color='red')
+plt.plot(meanFitnessValues, color='green')
+plt.xlabel('Generation')
+plt.ylabel('Min / Average Fitness')
+plt.title('Min and Average fitness over Generations')
+# show both plots:
+plt.show()
+
+# now plot the best travelling path.
+plt.scatter(*zip(*CITIES), marker='.', color='red')
+locs = [CITIES[i] for i in best]
+locs.append(locs[0])
+plt.plot(*zip(*locs), linestyle='-', color='blue')
